12118c8e4a81d0dc24e04378906f1f3c4b666665
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-01-19 09:00:34 -0300

    correção do erro da hora e segundos.

--- RelogioDigital/js/script.js
+++ RelogioDigital/js/script.js
@@ -9,8 +9,8 @@ const relogio = setInterval(function time(){
   let s = dateToday.getSeconds();
 
 if (hr < 10) hr = '0' + hr;
-if (min < 10) hr = '0' + min;
-if (s < 10) hr = '0' + s;
+if (min < 10) min = '0' + min;
+if (s < 10) s = '0' + s;
 
   horas.textContent = hr;
   minutos.textContent = min;
